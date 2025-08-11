8fba81c43ac66c80ed8bfca6e9ac68a0c4a8166e
Koz9vo4ka <yura.schetchkov@mail.ru>
2025-08-12 01:14:30 +0300

    add private function

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from drone_pygame_map import DroneDataVisualization
 
 class DroneMission:
     def __init__(self, drone_ip: str) -> None:  
-        self.start_time = time.time()      
+        self.__start_time = time.time()      
         self.__DRONE_IP = drone_ip
-        self.result = []
-        self.last_time_photo = 0
+        self.__result = []
+        self.__last_time_photo = 0
         
-        self.mission_plan = [
+        self.__mission_plan = [
             # go forward and back
             [4.0, 0.0],
             [0.5, 0.0],
@@ -36,19 +36,19 @@ class DroneMission:
             [],
         ]
         
-        self.client = Drone()
+        self.__client = Drone()
         self.pygame_visualization = DroneDataVisualization()
-        self.motion_machine = StateMachine(self.mission_plan, self.drone)
+        self.motion_machine = StateMachine(self.__mission_plan, self.__client)
         
 
-    def connect_to_drone(self) -> None:
-        print(self.drone.connect(self.drone_ip, reset_state=True))
+    def __connect_to_drone(self) -> None:
+        print(self.drone.connect(self.__DRONE_IP, reset_state=True))
 
-    def taking_off(self, altitude) -> None:
+    def __taking_off(self, altitude: float) -> None:
         self.drone.set_height(altitude)
         print(f"TAKE OFF!! set_height = {altitude}")
     
-    def process_detections_and_display(self):
+    def __process_detections_and_display(self) -> None:
         img = self.drone.get_image()
         x, y = self.drone.get_nav_pose()
         yaw = self.drone.get_rpy()[2]
@@ -63,32 +63,31 @@ class DroneMission:
             cv2.imwrite(f"photos/{round(x, 2)}|{round(y, 2)}|{round(yaw, 4)}|{round(time.time(), 3)}.png", img)
         
         cv2.imshow("video", img)
-        return x, y, yaw
     
-    def check_mission_status(self):
+    def __check_mission_status(self) -> bool:
         if self.motion_machine.is_mission_end():
             self.drone.landing_nb()
             print("LANDING!!")
             return False
         return True
     
-    def periodic_save_detections(self):
+    def __periodic_save_detections(self) -> None:
         if abs(self.last_time_photo - time.time()) > 3:
             peoples, fires = obj_manager.get_sorted_detections()
             obj_manager.save_detections(peoples, fires)
             self.last_time_photo = time.time()
     
-    def run_mission(self):
-        self.connect_to_drone()
-        self.taking_off(1.5)
+    def run_mission(self) -> None:
+        self.__connect_to_drone()
+        self.__taking_off(1.5)
         
         while True:
-            self.process_detections_and_display()
+            self.__process_detections_and_display()
             
-            if not self.check_mission_status():
+            if not self.__check_mission_status():
                 break
                 
-            self.periodic_save_detections()
+            self.__periodic_save_detections()
             
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
